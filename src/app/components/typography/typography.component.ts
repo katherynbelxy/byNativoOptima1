8c728f8f678b26078b115bc3cae94d59fa3414ad
Manuel <manuelitoys93@gmail.com>
2020-09-03 12:08:46 -0500

    efecto tipografia

--- src/app/components/typography/typography.component.ts
+++ src/app/components/typography/typography.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-typography',
@@ -40,6 +41,21 @@ clickparpadeo(){
 }
 
   ngOnInit(): void {
+    $('td').on('mouseenter', function(){
+      var a = $(this).prev('td').attr('aria-label')
+      var b = $(this).attr('aria-label')
+      var c = $(this).next('td').attr('aria-label')
+      $('td').removeClass('rojito')
+      $(this).addClass('rojito')
+      $(this).next('td').addClass('rojito')
+      $(this).prev('td').addClass('rojito')
+      $(this).parent().prev('tr').find('.'+a).addClass('rojito')
+      $(this).parent().prev('tr').find('.'+b).addClass('rojito')
+      $(this).parent().prev('tr').find('.'+c).addClass('rojito')
+      $(this).parent().next('tr').find('.'+a).addClass('rojito')
+      $(this).parent().next('tr').find('.'+b).addClass('rojito')
+      $(this).parent().next('tr').find('.'+c).addClass('rojito')
+    })
   }
 
 }
